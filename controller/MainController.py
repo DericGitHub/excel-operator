dd090835ec96cc3413ed7dc10d228cecb84d612b
Deric <916856445@qq.com>
2017-08-02 00:22:21 +0800

    20170802

--- controller/MainController.py
+++ controller/MainController.py
@@ -15,12 +15,19 @@ class MainController(object):
         self._PSstack = None
         self._CASstack = None
         self.init_GUI()
+        self.bind_GUI_event()
+        self.show_GUI()
     
     def init_GUI(self):
         self._application = QApplication(sys.argv)
         self._window = Window.Window()
+    def bind_GUI_event(self):
+        self._window.bind_open_cas(self.testfunc)
+    def show_GUI(self):
         self._window.show()
-        sys.exit(self._application.exec_())
+        sys.exit(self._application.exec_())       
+    def testfunc(self):
+        print 'test'
         
 
 
--- view/Window.py
+++ view/Window.py
@@ -17,4 +17,16 @@ class Window(QMainWindow):
     def init_Window(self):
         self.ui = WindowUI.Ui_MainWindow()
         self.ui.setupUi(self)
-        
+    
+    ##################################################
+    #       Bind event
+    ##################################################
+    def bind_open_cas(self,func):
+        self.ui.open_cas.clicked.connect(func)
+        self.ui.open_cas.clicked.connect(self.open_file_dialog)
+
+    def open_file_dialog(self):
+        filedialog = QFileDialog()
+        #filedialog.show()
+        fileName = filedialog.getOpenFileName()
+        print fileName
