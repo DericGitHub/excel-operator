71c7560a5af09778512ab997cd1a0139b9b53ecf
Deric <916856445@qq.com>
2017-08-03 21:13:43 +0800

    20170803 halfway of structure change

--- model/PSbook.py
+++ model/PSbook.py
@@ -11,17 +11,13 @@ class PSbook(Workbook):
     #       Initial method
     ##################################################
     def __init__(self,file_name = None):
+        super(PSbook,self).__init__(file_name)
         if file_name != None:
-            self.init_book()
-        super(PSbook,self).__init__(self._workbook)
+            self.init_ps_book()
+    def init_ps_book(self):
+        #self._workbook = xl.load_workbook(file_name)
+        #self._current_sheet = None
         self.load_sheets(PSsheet,self._workbook.worksheets)
         self.load_sheets_name(self._workbook.sheetnames)
-    def init_book(self):
-        self._workbook = xl.load_workbook(file_name)
-        self._current_sheet = None
-        self.load_sheets(PSsheet,self._workbook.worksheets)
-        self.load_sheets_name(self._workbook.sheetnames)
-    def init_model(self):
-        self._sheet_name_model = QStandardItemModel()
 
 
--- model/Workbook.py
+++ model/Workbook.py
@@ -1,6 +1,8 @@
 import openpyxl as xl
 
 from Worksheet import Worksheet
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
 ##################################################
 #       abstract class to handle workbook
@@ -11,23 +13,23 @@ class Workbook(object):
     #       Initial method
     ##################################################
     def __init__(self,workbook = None):
-        self._workbook = workbook
+        if workbook != None:
+            self.init_book(workbook)
+        self.init_model()
+
+    def init_book(self,workbook):
+        self._workbook_name = workbook
         self._sheets = {}
         self._sheets_cnt = None
         self._sheets_name = []
-        if self._workbook != None:
-            self.init_book()
-        self.init_model()
-
-    def init_book(self):
-        self._workbook = xl.load_workbook(file_name)
+        self._workbook = xl.load_workbook(self._workbook_name)
         self._current_sheet = None
     def init_model(self):
         self._book_name = None
         self._sheet_name_model = QStandardItemModel()
 
-    def update_book_name(self,name):
-        self._book_name = name
+    def update_workbook_name(self,name):
+        self._workbook_name = name
     def update_sheet_name_model(self):
         self._sheet_name_model.clear()
         for sheet_name in self._sheets_name:
@@ -46,18 +48,6 @@ class Workbook(object):
         for sheet in sheets:
             self._sheets_name.append(sheet)
 
-    def init_book(self):
-        self._workbook = xl.load_workbook(file_name)
-        self._current_sheet = None
-    def init_model(self):
-        self._book_name = None
-        self._sheet_name_model = QStandardItemModel()
-
-    def update_book_name(self,name):
-        self._book_name = name
-    def update_sheet_name_model(self,model):
-        self._sheet_name_model.clear()
-        for sheet in 
     ##################################################
     #       user interface
     ##################################################
