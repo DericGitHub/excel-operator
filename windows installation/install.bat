8fa768c4bb81d30e71d1e77f5e3759d5c0dc7b2a
DericGitHub <916856445@qq.com>
2017-08-08 17:49:20 +0800

    20170808 add Windows requirement software installation script

---
+++ windows installation/install.bat
@@ -0,0 +1,8 @@
+@echo off
+echo 'Installing python for Windows'
+start /wait python-2.7.13.amd64.msi 
+echo 'Installed python for Windows'
+echo 'Installing PyQt for Windows'
+start /wait PyQt4-4.10-gpl-Py2.7-Qt4.8.4-x64.exe
+echo 'Installe PyQt for Windows'
+pause
