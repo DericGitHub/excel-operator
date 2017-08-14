31fcbc0d89731f305bec3323cdadea60b434870e
DericGitHub <916856445@qq.com>
2017-08-14 11:03:43 +0800

    20170814 Update Windows installation requirment

--- test1.py
+++ test1.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 import openpyxl as xl
 
 a = xl.load_workbook('Product Specification.xlsx')
 b = xl.writer.excel.save_virtual_workbook(a)
-f = open('2.xlsx','w')
+f = open('1.txt','w')
 f.write(b)
 f.close()
--- windows installation/install.bat
+++ windows installation/install.bat
@@ -4,5 +4,14 @@ start /wait python-2.7.13.amd64.msi
 echo 'Installed python for Windows'
 echo 'Installing PyQt for Windows'
 start /wait PyQt4-4.10-gpl-Py2.7-Qt4.8.4-x64.exe
-echo 'Installe PyQt for Windows'
+echo 'Installed PyQt for Windows'
+echo 'Installing pywin32'
+start /wait pywin32-220.win-amd64-py2.7.exe
+echo 'Installed pywin32'
+echo 'Installing openpyxl'
+pip install openpyxl
+echo 'Installed openpyxl'
+echo 'Installing xlwings'
+pip install xlwings
+echo 'Installed xlwings for Windows'
 pause
